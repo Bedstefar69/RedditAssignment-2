9873c75d30d11914deab183383ebe78e717afe7e
Bedstefar69 <andreastamsen@gmail.com>
2022-11-01 14:36:22 +0100

    Read fra json fiel

--- WebAPI/Services/AuthService.cs
+++ WebAPI/Services/AuthService.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using FileData;
 using Shared.Models;
 
 namespace WebAPI.Services;
@@ -6,15 +9,20 @@ namespace WebAPI.Services;
 public class AuthService : IAuthService
 
 {
+    private IList<User> ReadFile()
+    {
+        string jsonString = File.ReadAllText(@"./data.json");
+        DataContainer dc = JsonSerializer.Deserialize<DataContainer>(jsonString);
+        return dc.Users.ToList();
+    }
 
-    private readonly IList<User> userList = new List<User>
+
+    private readonly IList<User> users = new List<User>
     {
         new User
         {
             Username = "flemse",
-            Password = "1234"
-
-
+            Password = "flemming123"
         },
         new User
         {
@@ -25,7 +33,7 @@ public class AuthService : IAuthService
 
     public Task<User> ValidateUser(string username, string password)
     {
-        User? existingUser = userList.FirstOrDefault(u =>
+        User? existingUser = ReadFile().FirstOrDefault(u =>
             u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
 
         if (existingUser == null)
@@ -43,7 +51,6 @@ public class AuthService : IAuthService
 
     public Task RegisterUser(User user)
     {
-
         if (string.IsNullOrEmpty(user.Username))
         {
             throw new ValidationException("Username cannot be null");
@@ -57,10 +64,8 @@ public class AuthService : IAuthService
 
         // save to persistence instead of list
 
-        userList.Add(user);
+        ReadFile().Add(user);
 
         return Task.CompletedTask;
     }
-
-    
 }
