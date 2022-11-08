26bb053d93c47528da1abf7156f0be618815a90f
Bedstefar69 <andreastamsen@gmail.com>
2022-11-08 15:53:52 +0100

    Database integration v1

--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -2,11 +2,13 @@
 
 public class Post
 {
-    public int Id { get; set; }
-    public User Owner { get; }
-    public string Title { get; set; }
     
-    public string Body { get; set; }
+    
+    public int Id { get; set; }
+    public User Owner { get; private set; }
+    public string Title { get; private set; }
+    
+    public string Body { get; private set; }
   
     
 
@@ -15,5 +17,10 @@ public class Post
         Owner = owner;
         Title = title;
         Body = body;
+    }
+
+    private Post()
+    {
+        
     }
 }
---
+++ EFCDataAccess/DAOs/UserEfcDao.cs
@@ -0,0 +1,50 @@
+﻿using Application.DAOInterfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace EFCDataAccess.DAOs;
+
+public class UserEfcDao : IUserDAO
+{
+    private readonly PostContext context;
+
+    public UserEfcDao(PostContext context)
+    {
+        this.context = context;
+    }
+    public async Task<User> CreateUserAsync(User user)
+    {
+        EntityEntry<User> newUser = await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+        return newUser.Entity;
+    }
+
+    public async Task<User?> GetByUsernameAsync(string userName)
+    {
+        
+        User? existing = await context.Users.FirstOrDefaultAsync(u =>
+            u.Username.ToLower().Equals(userName.ToLower())
+        );
+        return existing;
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        User? user = await context.Users.FindAsync(id);
+        return user;
+    }
+
+    public async Task<IEnumerable<User>> GetAsync(GetUsersDTO searchParameters)
+    {
+        IQueryable<User> usersQuery = context.Users.AsQueryable();
+        if (searchParameters.UsernameContains != null)
+        {
+            usersQuery = usersQuery.Where(u => u.Username.ToLower().Contains(searchParameters.UsernameContains.ToLower()));
+        }
+
+        IEnumerable<User> result = await usersQuery.ToListAsync();
+        return result;
+    }
+}
